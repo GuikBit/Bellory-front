5c9062b68ca216d6aebe3fb2ac66e4ce772f02e3
Guilherme Olivera <guilherme.oliveira@viannasempre.com.br>
2025-05-01 16:40:47 -0300

    teste offline

--- src/route/Routing.tsx
+++ src/route/Routing.tsx
@@ -17,6 +17,19 @@ const LoadingFallback = () => (
 );
 
 function Routing() {
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then(
+        (registration) => {
+          console.log('SW registered: ', registration);
+        },
+        (registrationError) => {
+          console.log('SW registration failed: ', registrationError);
+        }
+      );
+    });
+  }
   
   return (
     <Router>
